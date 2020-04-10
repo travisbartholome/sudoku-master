480712f7efa14aac4e6e27a09b93387f451a507f
George Hodgkins <hodgkinsgeorge@gmail.com>
2020-04-10 13:47:54 -0500

    Added RAM instat and navigation logic

--- src/interfaceController.v
+++ src/interfaceController.v
@@ -10,10 +10,10 @@
 module interfaceController (
 
 	// user inputs
-	userNum, upButton, dnButton, leftButton, rightButton, writeBit,
+	userNum, upButton, downButton, leftButton, rightButton, writeBit,
 
 	// display output
-	currentRow,
+	currentRow, currentNum, noWrite,
 	
 	// memory interface I/Os
 	RomAddr, RomDat, RamAddr, RamDat, RamWriteBit,
@@ -35,7 +35,11 @@ module interfaceController (
 
 	//-----Display output-----
 	// current row to be displayed to the user (4 hex digits)
+	// tied to output of RAM
 	output [15:0] currentRow;
+	// current selected digit in the row (as a bit), tells display decoder which to blink
+	output [3:0] currentNum;
+	reg [3:0] currentNum;
 
 
 	//-----Memory interfaces----
@@ -50,15 +54,81 @@ module interfaceController (
 	//
 	// The RAM contains 4 16-bit words which define the current status of
 	// the game. Its values are loaded from the ROM on game start.
-	//
+
 	// ROM read address
 	output [3:0] RomAddr;
+	reg [3:0] RomAddr;
 	// Data read from ROM
 	input [19:0] RomDat;
-	// RAM R/W address
+	// write-protect bits from ROM
+	wire [3:0] writeProtect = RomDat[19:16];
+	// number data from ROM
+	wire [15:0] storedConfig = RomDat[15:0];
+
+	// RAM R/W address, selects the game row
 	output [1:0] RamAddr;
+	reg [1:0] RamAddr;
 	// RAM R/W bit
 	output RamWriteBit;
+	reg RamWriteBit;
 	// Data read from RAM
 	input [15:0] RamDat;
-	
+	// buffer reg because we have to write a whole word back into RAM,
+	// but we only want to change 4 bits of the word
+	reg [15:0] RamDatReg;
+
+	// instantiate RAM module
+	// RAM module is generated from a Quartus IP core
+	sudokuRAM RAM (
+		.address(RamAddr),
+		.clock(CLK),
+		.data(RamDatReg), // input data
+		.wren(RamWriteBit),
+		.q(RamDat)); // output data
+
+	// sequential logic for moving through stored values and writing new ones
+	always @(posedge CLK) begin
+		// synchronize RamDat buffer reg
+		RamDatReg <= RamDat;
+		if (RST) begin // positive-logic reset
+			RomAddr <= 0;
+			RamAddr <= 0;
+			currentNum <= 0;
+			RamWriteBit <= 0;
+		end
+		else begin
+			// write bit takes priority over move controls, so you
+			// can't move around if it's high (even if you're
+			// trying to write to a protected location)
+			if (writeBit) begin
+				// modify relevant bits of buffer reg
+				case (currentNum)
+					'h0: ramDatReg[3:0] <= userNum;
+					'h2: ramDatReg[7:4] <= userNum;
+					'h4: ramDatReg[11:8] <= userNum;
+					'h8: ramDatReg[15:12] <= userNum;
+					default: ramDatReg <= ramDatReg;
+				endcase
+				// only write if this loc is not write protected
+				RamWriteBit <= currentNum & !writeProtect;
+			end
+			else begin
+				RamWriteBit <= 0;
+				if (leftButton) begin
+					// can't use <</>> because we need rotation
+					currentNum <= {currentNum[2:0], currentNum[3]};
+				end
+				else if (rightButton) begin
+					currentNum <= {currentNum[0], currentNum[3:1]};
+				end
+				// addition & subtraction automatically wrap around
+				else if (upButton) begin
+					RamAddr <= RamAddr - 1;
+				end
+				else if (downButton) begin
+					RamAddr <= RamAddr + 1;
+				end
+			end
+		end
+	end
+
