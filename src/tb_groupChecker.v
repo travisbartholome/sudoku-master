288ac8b162c06dae89e02abdaa0fcf2b4b89e0d9
travisbartholome <travis.bartholome@gmail.com>
2020-04-21 22:42:00 -0500

    Update groupChecker testbench, add documentation screenshot

--- src/tb_groupChecker.v
+++ src/tb_groupChecker.v
@@ -7,29 +7,39 @@
 module tb_groupChecker();
   reg [15:0] groupDigits;
   wire groupCorrect;
+  reg CLK = 1'b0;
+  reg [7:0] cycleCounter = 0;
 
-  groupChecker DUT_groupChecker(groupDigits, groupCorrect);
+  groupChecker DUT_groupChecker(CLK, groupDigits, groupCorrect);
 
   initial begin
     groupDigits = 16'h0000;
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h1111; // Invalid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h1231; // Invalid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h1324; // Valid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h1221; // Invalid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h4411; // Invalid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h0231; // Invalid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h0024; // Invalid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h1423; // Valid
-    #100;
+    repeat (2) @(posedge CLK);
     groupDigits = 16'h1433; // Invalid
-    #100;
+    repeat (2) @(posedge CLK);
+  end
+  
+  // Test clock (50 MHz)
+  always begin
+    if (cycleCounter < 50) begin
+      #10; CLK <= !CLK;
+      cycleCounter <= cycleCounter + 1;
+    end
   end
 endmodule
