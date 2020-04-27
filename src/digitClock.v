bb2076678e75f4fb4b4c2b9c27f3ec2497a57b6c
Stefan Bucur <sbucur@uh.edu>
2020-04-27 14:40:07 -0500

    Add clock to src + instantiate in top module

---
+++ src/digitClock.v
@@ -0,0 +1,78 @@
+// ECE5440
+// Author: Stefan Bucur 3153
+// Module: digitClock
+// Description: scalable digit timer for the FPGA game.
+//  timer talks to adjacent digits to determine next value
+
+/*  Visual showing how the timer chains together.
+    Connect the larger-value digit to the left, lesser to the right.
+                     _____
+    borrow_up   <---|     |<--- borrow_dn
+                    | d_T |
+    noborrow_up --->|_____|---> noborrow_dn
+*/
+// the timer counts down via a pulse from its borrow_dn wire connected to either a 
+// less significant digit timer or a 1s clock cycle.
+
+// borrow == "i want to borrow"
+// noborrow == "you cannot borrow"
+
+module digitClock (
+    reconf, count_default,
+    borrow_up, borrow_dn,
+    noborrow_up, noborrow_dn,
+    count
+    );
+
+    input borrow_dn, noborrow_up, reconf;
+    input [3:0] count_default;
+    output borrow_up, noborrow_dn;
+    output [3:0] count;
+
+    reg borrow_up, noborrow_dn;
+    reg [3:0] count;
+
+    always @ (posedge borrow_dn, posedge reconf) begin
+        if (reconf == 1'b1)
+        begin
+            if(count_default > 9)
+            begin
+                count <= 4'b0000;
+                noborrow_dn <= 1'b0;
+            end
+            else begin
+                count <= count_default;
+                if(count_default == 0)
+                begin
+                    noborrow_dn <= 1'b1;
+                end
+                else begin
+                    noborrow_dn <= 1'b0;
+                end
+            end
+            borrow_up <= 1'b1;
+        end
+        else begin
+            if (count == 4'b1001)
+            begin
+                if (noborrow_up == 1'b0)
+                begin
+                    // back up to 0, signal to slave digit
+                    count <= 4'b0000;
+                    borrow_up <= 1'b1;
+                    noborrow_dn <= 1'b0;
+                end
+                else begin
+                    borrow_up <= 1'b1;
+                    noborrow_dn <= 1'b1;
+                end
+            end
+            else begin
+                // no edge cases here, subtract when asked by child timer (borrow_dn pulse)
+                count <= count + 4'b0001;
+                borrow_up <= 1'b0;
+                noborrow_dn <= 1'b0;
+            end
+        end
+    end
+endmodule
---
+++ src/digitClock_2.v
@@ -0,0 +1,38 @@
+// ECE5440
+// Author: Stefan Bucur 3153
+// Module: digitClock_2
+// Description: two-digit timer for use in an FPGA game.
+/*  Visual showing how the timers chain together.
+    Larger-value digit from the left, lower from right.
+                     _____                  _____
+      borrow_up <---|     |<-borrow_mid----|     |<--- borrow_dn
+                    | ten |                | one |
+    noborrow_up --->|_____|--noborrow_mid->|_____|---> noborrow_dn
+*/
+module digitClock_2(
+    reconf, count_default1, count_default2,
+    borrow_up, borrow_dn,
+    noborrow_up, noborrow_dn,
+    count_tens, count_ones
+    );
+
+    input reconf, borrow_dn, noborrow_up;
+    input [3:0] count_default1, count_default2;
+    //connect the two digitClocks together
+    wire borrow_mid, noborrow_mid;
+    output borrow_up, noborrow_dn;
+    output [3:0] count_tens, count_ones;
+
+    digitClock tens(
+        reconf, count_default1,
+        borrow_up, borrow_mid,
+        noborrow_up, noborrow_mid,
+        count_tens
+    );
+    digitClock ones(
+        reconf, count_default2,
+        borrow_mid, borrow_dn,
+        noborrow_mid, noborrow_dn,
+        count_ones
+    );
+endmodule
