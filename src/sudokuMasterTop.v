7d1092d01257866d9adf1177559629a211322ecb
jiggygenty <jonxgen@hotmail.com>
2020-05-05 18:06:51 -0500

    I completed the instantiations of my modules and setup the test bench module for the top module

--- src/sudokuMasterTop.v
+++ src/sudokuMasterTop.v
@@ -7,11 +7,12 @@
 module sudokuMasterTop (
 
 	// inputs
-	userNum, upButton, downButton, leftButton, rightButton, writeSwitch,
+	userNum, b_upButton, b_downButton, b_leftButton, b_rightButton, writeSwitch,
 
 	// outputs
 	userNumDisp, wpInd, rowDisp, winInd,
-
+	//additional output
+	rowNums, currentNum,
 	// system clock and reset
 	CLK, RST);
 
@@ -23,7 +24,7 @@ module sudokuMasterTop (
 	input [3:0] userNum;
 
 	// navigation buttons
-	input upButton, downButton, leftButton, rightButton;
+	input b_upButton, b_downButton, b_leftButton, b_rightButton;
 
 	// R/W switch
 	input writeSwitch;
@@ -37,11 +38,23 @@ module sudokuMasterTop (
 	output wpInd;
 
 	// 4 7-segs showing current row
-	output [23:0] rowDisp;
+	output [27:0] rowDisp;
 
 	// win indicator
 	output winInd;
 
+	//-----additional output declaration for debugging purposes//
+	output [15:0] rowNums;
+	output [3:0] currentNum;
+	//-----end of additional output declaration----------------//
+
+	// button shaper instats
+	// internal signals for the interface controller //
+	wire upButton, downButton, leftButton, rightButton;
+	button_shaper upBtt_shaper(CLK,RST,b_upButton,upButton);
+	button_shaper downBtt_shaper(CLK,RST,b_downButton,downButton);
+	button_shaper leftBtt_shaper(CLK,RST,b_leftButton,leftButton);
+	button_shaper rightBtt_shaper(CLK,RST,b_rightButton,rightButton);
 
 	// interface controller instat
 	// hex inputs to 7-seg decoders
@@ -104,12 +117,19 @@ module sudokuMasterTop (
         .noborrow_up(1),
         .noborrow_dn(borrow_end2),
         .count_tens(time_tens),
-        .count_ones(time_ones),
+        .count_ones(time_ones)
     );
 		
 //digitBlinker dummy instant
 		wire isDigitSelected;
 		wire [6:0] digitInfo, digitOutput;
-digitBliker blinker(isDigitSelected,digitInfo,digitOutput,CLK,RST);
+digitBlinker blinker(isDigitSelected,digitInfo,digitOutput,CLK,RST);
 
+	// seven segment decoder instantiations
+	Seven_Seg userNumDisp_SS(currentNum,userNumDisp);
+
+	Seven_Seg rowDisp1_SS(rowNums[3:0],rowDisp[6:0]);
+	Seven_Seg rowDisp2_SS(rowNums[7:4],rowDisp[13:7]);
+	Seven_Seg rowDisp3_SS(rowNums[11:8],rowDisp[20:14]);
+	Seven_Seg rowDisp4_SS(rowNums[15:12],rowDisp[27:21]);
 endmodule
--- src/tb_sudokuMasterTop.v
+++ src/tb_sudokuMasterTop.v
@@ -1,1 +1,28 @@
+// ECE 5367
+// Group: Sudoku Master
+//
+// This module simulates the top-level module for the Sudoku Master
+// game.
 
+module tb_sudokuMasterTop ();
+	reg CLK, RST;	
+
+	reg [3:0] userNum;
+
+	reg upButton, downButton, leftButton, rightButton;
+
+	reg writeSwitch;
+	
+	wire [6:0] userNumDisp;
+
+	wire wpInd;
+
+	wire [27:0] rowDisp;
+
+	wire winInd;
+
+	wire [15:0] rowNums;
+	wire [3:0] currentNum;
+
+	sudokuMasterTop DUT_sudokuMasterTop(userNum,upButton,downButton,leftButton,rightButton,writeSwitch,userNumDisp,wpInd,rowDisp,winInd,rowNums,currentNum,CLK,RST);
+endmodule
